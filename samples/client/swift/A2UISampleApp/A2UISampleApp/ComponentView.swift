dd7fe5cd22ecfd97f12784e92801534a4f7d5d6a
sunny <sunny@riddimsoftware.com>
2026-02-25 13:55:40 -0500

    GEMINI: fix bindings in ComponentView
    PROMPT: i'm getting some Binding errors in ComponentView.swift, can you see what's going on

--- samples/client/swift/A2UISampleApp/A2UISampleApp/ComponentView.swift
+++ samples/client/swift/A2UISampleApp/A2UISampleApp/ComponentView.swift
@@ -3,7 +3,11 @@ import A2UI
 
 struct ComponentView: View {
 	@Environment(A2UIDataStore.self) var dataStore
-	let component: GalleryComponent
+	@State private var component: GalleryComponent
+	
+	init(component: GalleryComponent) {
+		self._component = State(initialValue: component)
+	}
 	
 	var body: some View {
 		VStack {
@@ -12,12 +16,42 @@ struct ComponentView: View {
 				.background(Color(.systemBackground))
 				.cornerRadius(12)
 				.shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 2)
+			
+			if !component.properties.isEmpty {
+				VStack(alignment: .leading, spacing: 10) {
+					ForEach($component.properties) { prop in
+						HStack {
+							Text(prop.wrappedValue.label)
+								.font(.subheadline)
+								.foregroundColor(.secondary)
+							Spacer()
+							Picker(prop.wrappedValue.label, selection: prop.value) {
+								ForEach(prop.wrappedValue.options, id: \.self) { option in
+									Text(option).tag(option)
+								}
+							}
+							.pickerStyle(.menu)
+							.onChange(of: prop.wrappedValue.value) {
+								updateSurface(for: component)
+							}
+						}
+					}
+				}
+				.padding()
+				.background(Color(.secondarySystemBackground))
+				.cornerRadius(10)
+			}
 		}
 		.onAppear {
 			dataStore.process(chunk: component.a2ui)
 			dataStore.flush()
 		}
 		.navigationTitle(component.id)
+	}
+	
+	private func updateSurface(for component: GalleryComponent) {
+		dataStore.process(chunk: component.updateComponentsA2UI)
+		dataStore.flush()
 	}
 }
 
