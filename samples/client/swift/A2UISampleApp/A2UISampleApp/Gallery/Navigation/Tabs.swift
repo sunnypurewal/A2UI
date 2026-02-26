d1ef284b159692f67849c8a2ffb0e2165d0b27b3
sunny <sunny@riddimsoftware.com>
2026-02-26 12:27:30 -0500

    add tabs to gallery

--- samples/client/swift/A2UISampleApp/A2UISampleApp/Gallery/Navigation/Tabs.swift
+++ samples/client/swift/A2UISampleApp/A2UISampleApp/Gallery/Navigation/Tabs.swift
@@ -5,11 +5,9 @@ extension GalleryComponent {
 	static let tabs: Self = {
 		return .init(
 			id: "Tabs",
-			template: #"{"id":"gallery_component","component":{"Tabs":{"children":["tab1_content","tab2_content"],"selection":{"path":"/tab"}}}}"#,
+			template: #"{"id":"gallery_component","component":{"Tabs":{"tabs":[{"title":"Tab 1","child":"tab1_content"},{"title":"Tab 2","child":"tab2_content"}]}}}"#,
 			staticComponents: [.root, .tab1, .tab2],
-			dataModelFields: [
-				.init(path: "/tab", label: "Selected Tab", value: .number(0)),
-			],
+			dataModelFields: [],
 			properties: []
 		)
 	}()
