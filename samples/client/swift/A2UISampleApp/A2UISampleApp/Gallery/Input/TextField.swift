32e4218a7ede32949fa253910582f7bf9480db02
sunny <sunny@riddimsoftware.com>
2026-02-26 13:42:05 -0500

    fix textfield validation gallery

--- samples/client/swift/A2UISampleApp/A2UISampleApp/Gallery/Input/TextField.swift
+++ samples/client/swift/A2UISampleApp/A2UISampleApp/Gallery/Input/TextField.swift
@@ -5,7 +5,7 @@ extension GalleryComponent {
 	static let textField: Self = {
 		return .init(
 			id: "TextField",
-			template: #"{"id":"gallery_component","checks":[{"condition":{"functionCall":{"call":"email","args":{"value":{"path":"/body/text"}}}},"message":"validation failed"}],"component":{"TextField":{"value":{"path":"/body/text"},"label":{"path":"/label"},"variant":"{{\#(textFieldVariantKey)}}"}}}"#,
+			template: #"{"id":"gallery_component","checks":[{"condition":{"call":"email","args":{"value":{"path":"/body/text"}}},"message":"validation failed"}],"component":{"TextField":{"value":{"path":"/body/text"},"label":{"path":"/label"},"variant":"{{\#(textFieldVariantKey)}}"}}}"#,
 			staticComponents: [.textFieldRoot, .body, .textFieldPreview],
 			dataModelFields: [
 				DataModelField(path: "/label", label: "Placeholder", value: .string("Enter text")),
